cc28f2969de9105cc8d3692cddd161f24db0b66a
wczapkowicz <66492122+wczapkowicz@users.noreply.github.com>
2022-04-29 20:25:14 +0200

    scss env

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from sassutils.wsgi import SassMiddleware
 
 app = Flask(__name__)
+
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/sass', 'static/css', '/static/css')
+})
 
 from app import routes
 
